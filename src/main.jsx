ff4a055ce7496fc69879e07f41d6a0ed261c94d9
manthrisairam-363 <75235650+manthrisairam-363@users.noreply.github.com>
2026-05-31 22:59:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// Lock orientation to portrait (works on Android + some iOS)
+if (screen?.orientation?.lock) {
+  screen.orientation.lock('portrait').catch(() => {})
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
